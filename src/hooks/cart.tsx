792cb074c8baf8149ec6cf5ee8dbbbb747a3c52b
Reginaldo Boeke <reginaldoboeke@MacBook-Pro-de-Reginaldo.local>
2020-09-14 21:09:55 -0300

    Finalizadas as func. do desafio

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -28,24 +28,83 @@ const CartContext = createContext<CartContext | null>(null);
 const CartProvider: React.FC = ({ children }) => {
   const [products, setProducts] = useState<Product[]>([]);
 
+  const storeCartProducts = useCallback((cartProducts: Product[]) => {
+    AsyncStorage.setItem('@GoMarketplace:cart', JSON.stringify(cartProducts));
+  }, []);
+
+  const addToCart = useCallback(
+    async product => {
+      const productIndex = products.findIndex(p => p.id === product.id);
+
+      if (productIndex < 0) {
+        const newProducts = [...products, { ...product, quantity: 1 }];
+
+        setProducts(newProducts);
+        storeCartProducts(newProducts);
+        return;
+      }
+
+      const newProducts = products.map((p, index) => ({
+        ...p,
+        quantity: index === productIndex ? p.quantity + 1 : p.quantity,
+      }));
+
+      setProducts(newProducts);
+      storeCartProducts(newProducts);
+    },
+    [products, storeCartProducts],
+  );
+
+  const increment = useCallback(
+    async id => {
+      const newProducts = products.map(product => ({
+        ...product,
+        quantity: product.id === id ? product.quantity + 1 : product.quantity,
+      }));
+
+      setProducts(newProducts);
+      storeCartProducts(newProducts);
+    },
+    [products, storeCartProducts],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const productSelected = products.find(product => product.id === id);
+
+      if (!productSelected) {
+        return;
+      }
+
+      if (productSelected.quantity === 1) {
+        const newProducts = products.filter(product => product.id !== id);
+
+        setProducts(newProducts);
+        storeCartProducts(newProducts);
+        return;
+      }
+
+      const newProducts = products.map(product => ({
+        ...product,
+        quantity: product.id === id ? product.quantity - 1 : product.quantity,
+      }));
+
+      setProducts(newProducts);
+      storeCartProducts(newProducts);
+    },
+    [products, storeCartProducts],
+  );
+
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storedProducts = await AsyncStorage.getItem('@GoMarketplace:cart');
+
+      if (storedProducts) {
+        setProducts(JSON.parse(storedProducts));
+      }
     }
 
     loadProducts();
-  }, []);
-
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
-
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
-
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
   }, []);
 
   const value = React.useMemo(
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-import { View, Image } from 'react-native';
+import { View, Image, Alert } from 'react-native';
 
 import formatValue from '../../utils/formatValue';
 import { useCart } from '../../hooks/cart';
@@ -35,14 +35,19 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      try {
+        const response = await api.get('products');
+        setProducts(response.data);
+      } catch (err) {
+        Alert.alert('Ops!', 'Problemas ao buscar produtos.')
+      }
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
